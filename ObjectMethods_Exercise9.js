da7dce35d8e273ba2505d7daf39a9ddf0d4b1f49
Arshya <codingbyarshya@gmail.com>
2023-03-15 11:30:24 +0300

    ObjectMethods_Exercise9

---
+++ ObjectMethods_Exercise9.js
@@ -0,0 +1,11 @@
+const person={
+firstName: "Mario",
+lastName: "Rossi",
+age: 25
+};
+const keys=Object.keys(person);
+console.log(keys);
+const values=Object.values(person);
+console.log(values);
+//in this exercise i used both Object.keys and also Object.values as the name they have, one of them give keys of the object for output 
+//the next one give values of the object for output.
